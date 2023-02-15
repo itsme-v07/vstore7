45f1b2cd06861218a15f62d663055293ab289361
itsme-v07 <geansyahip30@gmail.com>
2023-02-15 14:16:53 +0700

    using app wide state

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,16 +1,56 @@
+import 'package:provider/provider.dart';
+
 import 'package:flutter/material.dart';
+import 'package:vstore/providers/products.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum Filters {
+  Favorite,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('VStore'),
+        // filtering logic app wide state
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (Filters selectedValue) {
+              setState(() {
+                if (selectedValue == Filters.Favorite) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: Filters.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: Filters.All,
+              ),
+            ],
+          )
+        ],
         // backgroundColor: Color.fromARGB(255, 12, 0, 82),
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
