09296695fb2e9dc080c72d9d6f7510056f920d23
itsme-v07 <geansyahip30@gmail.com>
2023-02-15 14:17:22 +0700

    app wide state

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -4,6 +4,10 @@ import '../providers/products.dart';
 import '../widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
   // const ProductsGrid({
   //   Key? key,
   //   required this.loadProducts,
@@ -14,7 +18,8 @@ class ProductsGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products =
+        showFavs ? productsData.favoritesItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
